1fae8a5ead470a0d4ecd1a9e119ae067f5fb1e1b
cam <cameronayres123@gmail.com>
2025-01-30 03:58:18 +0000

    Started on send_message procedure.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![allow(unused)]
-use std::io;
 use std::env;
+use std::io;
+use std::io::prelude::*;
 use std::net::*;
 
 
@@ -8,16 +9,14 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let ip: Ipv4Addr = args[1].parse().expect("ip is in an invalid form.");
     let port: u16 = args[2].parse().unwrap();
-
-    println!("ip is {ip}, and the port is {port}.");
-    println!("The address is {}.", build_address(ip, port));
+    let full_address = build_address(ip, port);
 
     loop {
 	let mut buffer = String::new();
 	match io::stdin().read_line(&mut buffer) {
 	    Ok(n) => {
 		if(buffer == "exit\n".to_string()) { break };
-		print!("{buffer}");
+		send_message(&buffer);
 	    }
 	    
 	    Err(error) => println!("{error}"),
@@ -32,4 +31,10 @@ fn build_address(address: Ipv4Addr, port: u16) -> String {
     addr_and_port.push(':');
     addr_and_port.push_str(&port.to_string());
     addr_and_port
+}
+
+fn send_message(input: &String) -> std::io::Result<()> {
+    let mut stream = TcpStream::connect(input)?;
+    stream.write(input.as_bytes());
+    Ok(())
 }
